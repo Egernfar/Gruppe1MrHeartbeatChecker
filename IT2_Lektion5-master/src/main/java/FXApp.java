38b223072b93c135b248c769d7af2bcccaa581de
Egernfar <100682678+Egernfar@users.noreply.github.com>
2022-03-01 11:32:20 +0100

    Add files via upload

---
+++ IT2_Lektion5-master/src/main/java/FXApp.java
@@ -0,0 +1,42 @@
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Polyline;
+import javafx.stage.Stage;
+
+public class FXApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Button button = new Button("Draw a graph");
+        Polyline polyline = new Polyline();
+
+        button.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                for (int i = 0; i < 40; i++) {
+                    polyline.getPoints().addAll(i*5.0 , Math.random() * 480);
+                    try {
+                        Thread.sleep(250);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        AnchorPane anchorPane = new AnchorPane();
+        anchorPane.getChildren().addAll(button,polyline);
+        Scene scene = new Scene(anchorPane,640,480);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
